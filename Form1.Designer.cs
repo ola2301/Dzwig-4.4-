b1ee6c3fcfc4831aa26a41d79c939212500969f6
ola2301 <ola.g2301@gmail.com>
2019-06-24 22:21:41 +0200

    Rysowanie bloczków, przesuwanie

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace p4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Enabled = true;
+            this.moveTimer.Tick += new System.EventHandler(this.MoveTimer_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(261, 506);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 506);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(483, 506);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(101, 417);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Stwórz towar nr.1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.CheckBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(43, 462);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(85, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Złap/Upuść";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(261, 417);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(108, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Stwórz towar nr.2";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.CheckBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(405, 417);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(108, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "Stwórz towar nr.3";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.CheckBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(565, 417);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(108, 17);
+            this.checkBox5.TabIndex = 11;
+            this.checkBox5.Text = "Stwórz towar nr.4";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.CheckBox5_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 436);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "50";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(261, 441);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "50";
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(405, 441);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "50";
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(565, 440);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.Text = "50";
+            this.textBox4.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(727, 440);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = "600";
+            this.textBox5.TextChanged += new System.EventHandler(this.TextBox5_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(727, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Limit wagi (kg)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 541);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Drawing.Drawing2D;
+
+namespace p4
+{
+ //   public class Towar
+//    {
+  //      public int x;
+ //       public int y;
+ //
+ //       public void draw()
+  //      {
+//
+  //      }
+  //  }
+    public partial class Form1 : Form
+    {
+
+        int x0 = 100;
+        int y0 = 300;
+        int x1 = 100;
+        int y1 = 350;
+        bool draw1 = false;
+        bool draw2 = false;
+        bool draw3 = false;
+        bool draw4 = false;
+        bool visited = false;
+        bool grab = false;
+        int id = 0;
+        int tx1 = 120;
+        int ty1 = 380;
+
+        int tx2 = 160;
+        int ty2 = 380;
+
+        int tx3 = 200;
+        int ty3 = 380;
+
+        int tx4 = 240;
+        int ty4 = 380;
+        string s_w1, s_w2, s_w3, s_w4;
+        int w1 = 0;
+        int w2 = 0;
+        int w3 = 0;
+        int w4 = 0;
+        int limit = 600;
+        string s_limit;
+
+        //     List<Towar> towary = new List<Towar>
+        //          {
+        //              new Towar{ x = 120, y= 380 },
+        //              new Towar{ x = 160, y= 380 },
+        //              new Towar{ x = 200, y= 380 },
+        //              new Towar{ x = 240, y= 380 }
+        //          };
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // Rectangle noga = new Rectangle(50, 100, 30, 300);
+            //  Rectangle ramie = new Rectangle(20, 70, 400, 30);
+            // g.DrawRectangle(myPen, noga);
+            // g.DrawRectangle(myPen, ramie);
+
+            // g.Clear(Color.White);
+            // g.Dispose();
+
+
+            //Rectangle noga = new Rectangle(50, 100, 30, 300);
+            // Rectangle ramie = new Rectangle(20, 70, 400, 30);
+            // Pen myPen2 = new Pen(System.Drawing.Color.Blue);
+            // Rectangle towar = new Rectangle(tx0, ty0, 20, 20);
+
+            //g.DrawRectangle(myPen, noga);
+            //  g.DrawRectangle(myPen, ramie);
+            //  g.DrawLine(myPen, x0, y0, x1, y1);
+        }
+      
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics g;
+            g = this.CreateGraphics();
+            Pen myPen = new Pen(System.Drawing.Color.Black);
+            Pen myPen2 = new Pen(System.Drawing.Color.Red);
+
+            Rectangle noga = new Rectangle(50, 200, 30, 300);
+            Rectangle ramie = new Rectangle(20, 170, 400, 30);
+
+          
+            Rectangle towar1 = new Rectangle(tx1, ty1, 20, 20);
+            Rectangle towar2 = new Rectangle(tx2, ty2, 20, 20);
+            Rectangle towar3 = new Rectangle(tx3, ty3, 20, 20);
+            Rectangle towar4 = new Rectangle(tx4, ty4, 20, 20);
+
+
+            e.Graphics.DrawRectangle(myPen, noga);
+            e.Graphics.DrawRectangle(myPen, ramie);
+            e.Graphics.DrawLine(myPen, 0, 400, 1000, 400);
+            g.DrawLine(myPen, x0, y0, x1, y1);
+            if(draw1==true)
+            {
+                e.Graphics.DrawRectangle(myPen, towar1);
+            }
+            if (draw2 == true)
+            {
+                e.Graphics.DrawRectangle(myPen, towar2);
+            }
+            if (draw3 == true)
+            {
+                e.Graphics.DrawRectangle(myPen, towar3);
+            }
+            if (draw4 == true)
+            {
+                e.Graphics.DrawRectangle(myPen, towar4);
+            }
+
+            if ((y1 == 380) && (x0 < tx1 + 20) && (x0 > tx1) && (draw1 == true) && checkBox2.Checked)
+            {
+                id = 1;
+                visited = true;
+            }
+            
+            if( (y1 == 380) && (x0 < tx2 + 20) && (x0 > tx2) && (draw2 == true) && checkBox2.Checked)
+            {
+                id = 2;
+                visited = true;
+            }
+            if ((y1 == 380) && (x0 < tx3 + 20) && (x0 > tx3) && (draw3 == true) && checkBox2.Checked)
+            {
+                id = 3;
+                visited = true;
+            }
+            if ((y1 == 380) && (x0 < tx4 + 20) && (x0 > tx4) && (draw4 == true) && checkBox2.Checked)
+            {
+                id = 4;
+                visited = true;
+            }
+            
+        }
+
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (y1 > 110)
+            {
+                y1 -= 5;
+            }
+            if(visited==true && grab==true)
+            {
+                switch (id)
+                {
+                    case 1: ty1 -= 5;
+                        break;
+                    case 2: ty2 -= 5;
+                        break;
+                    case 3: ty3 -= 5;
+                        break;
+                    case 4: ty4 -= 5;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (x0 > 85)
+            {
+                x0 -= 5;
+                x1 -= 5;
+            }
+            if (visited == true && grab == true)
+            {
+                switch (id)
+                {
+                    case 1:
+                        tx1 -= 5;
+                        break;
+                    case 2:
+                        tx2 -= 5;
+                        break;
+                    case 3:
+                        tx3 -= 5;
+                        break;
+                    case 4:
+                        tx4 -= 5;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (y1 < 390)
+            {
+                y1 += 5;
+            }
+            if (visited == true && grab == true)
+            {
+                switch (id)
+                {
+                    case 1:
+                        ty1 += 5;
+                        break;
+                    case 2:
+                        ty2 += 5;
+                        break;
+                    case 3:
+                        ty3 += 5;
+                        break;
+                    case 4:
+                        ty4 += 5;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+                {
+                    if (x0 < 400)
+                    {
+                        x0 += 5;
+                        x1 += 5;
+                    }
+                    if (visited == true && grab == true)
+                    {
+                        switch (id)
+                        {
+                            case 1:
+                                tx1 += 5;
+                                break;
+                            case 2:
+                                tx2 += 5;
+                                break;
+                            case 3:
+                                tx3 += 5;
+                                break;
+                            case 4:
+                                tx4 += 5;
+                                break;
+                            default: break;
+                        }
+
+                    }
+                }
+
+        private void CheckBox1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                draw1 = true;
+            }
+            if(!checkBox1.Checked)
+            {
+                draw1 = false;
+            }
+        }
+
+        private void CheckBox2_CheckedChanged(object sender, EventArgs e)
+       {
+           if(checkBox2.Checked)
+          {
+               if (w1 <= limit) grab = true;
+               else grab = false;
+           }
+           if (!checkBox2.Checked)
+            {
+              grab = false;
+                visited = false;
+           }
+        }
+
+
+        private void CheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                draw2 = true;
+            }
+            if (!checkBox3.Checked)
+            {
+                draw2 = false;
+            }
+        }
+
+        private void CheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                draw3 = true;
+            }
+            if (!checkBox4.Checked)
+            {
+                draw3 = false;
+            }
+        }
+
+        private void CheckBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked)
+            {
+                draw4 = true;
+            }
+            if (!checkBox5.Checked)
+            {
+                draw4 = false;
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            s_w1 = textBox1.Text;
+            w1 = int.Parse(s_w1);
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+            s_limit= textBox5.Text;
+            limit = int.Parse(s_limit);
+        }
+
+
+
+        private void CheckBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            s_w2 = textBox2.Text;
+            w2 = int.Parse(s_w2);
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            s_w3 = textBox3.Text;
+            w3 = int.Parse(s_w3);
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+            s_w4 = textBox4.Text;
+            w4 = int.Parse(s_w4);
+        }
+
+    }
+}
